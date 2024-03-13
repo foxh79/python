d2baad21ef4ec3a2f38daf7566d23f9bf438cf07
Boniface Mwau <160026270+foxh79@users.noreply.github.com>
2024-03-14 00:03:48 +0300

    commit

---
+++ app.py
@@ -0,0 +1,13 @@
+import math 
+my_name = "Foxh Mwau"
+print(my_name)
+
+good_credit = True
+has_high_income = True
+
+if good_credit or has_high_income: 
+    print("You get the loan!")
+else:
+    print("Sorry, you don't qualify for a loan")
+
+print(math.lcm(12, 4, 2))
