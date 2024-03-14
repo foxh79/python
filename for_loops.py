1f7e6fd61a22ede6900c0e9ca11ca2737f476d9d
Boniface Mwau <160026270+foxh79@users.noreply.github.com>
2024-03-15 00:42:46 +0300

    commit

---
+++ for_loops.py
@@ -0,0 +1,12 @@
+for item in "Foxh":
+    print(item)
+
+for item in ["Foxh", "Mwau", "Boniface"]:
+    print(item)
+
+prices = [71, 73, 79]
+
+total = 0
+for price in prices:
+    total += price
+print(f"Total: {total}")
