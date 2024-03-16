9e648c154dbc9e63c787c38f36c5c8e90a069fa8
Boniface Mwau <160026270+foxh79@users.noreply.github.com>
2024-03-16 08:57:46 +0300

    Update conditionals.py

--- conditionals.py
+++ conditionals.py
@@ -13,3 +13,8 @@ if good_credit and has_high_income:
 else:
     print("sorry, you dont qualify for a loan")
     
+#logical operator not
+if not good_credit and not has_high_income:
+    print("sorry, you dont qualify for a loan")
+else:
+    print("You get the loan!")
