2177a0c0fbbc19432c6da80ec0ff07b594eea954
Boniface Mwau <160026270+foxh79@users.noreply.github.com>
2024-03-16 08:53:52 +0300

    Update conditionals.py

--- conditionals.py
+++ conditionals.py
@@ -1,9 +1,11 @@
 good_credit = True
 has_high_income = True
 
-#logical operators
+#logical operator or
 if good_credit or has_high_income: 
     print("You get the loan!")
 else:
     print("Sorry, you don't qualify for a loan")
+
+
 
