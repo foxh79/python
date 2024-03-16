1509898f45b5cc4e726356f7ede7dacd408a2b4d
Boniface Mwau <160026270+foxh79@users.noreply.github.com>
2024-03-16 08:51:16 +0300

    Update app.py

--- app.py
+++ app.py
@@ -5,12 +5,5 @@ import math
 my_name = input("Foxh Mwau")
 print(my_name)
 
-good_credit = True
-has_high_income = True
-
-if good_credit or has_high_income: 
-    print("You get the loan!")
-else:
-    print("Sorry, you don't qualify for a loan")
-
+#ussing math library
 print(math.lcm(12, 4, 2))
