16dcfa9426f751d92527a99e6bd17061a8253932
Boniface Mwau <160026270+foxh79@users.noreply.github.com>
2024-03-16 11:52:20 +0300

    Add files via upload

---
+++ largest_number_in_list.py
@@ -0,0 +1,20 @@
+numbers = [1, 2, 3, 4, 5, 6, 7, 14]
+
+# Initialize the maximum number to the smallest possible value
+max_number = numbers[0]
+
+# Iterate over the list of numbers
+for number in numbers:
+    # If the current number is greater than the maximum number, update the
+    # maximum number to the current number
+    if number > max_number:
+        max_number = number
+
+# Print the maximum number
+print(f"Maximum number is {max_number}")
+
+#alternatively using the max() function
+max_number = max(numbers)
+
+# Print the maximum number
+print(f"Maximum number using math funttion is {max_number}")
