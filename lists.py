8d039e3c0bbac9b3f63a488357d96d995de804ba
Boniface Mwau <160026270+foxh79@users.noreply.github.com>
2024-03-16 11:53:30 +0300

    commit

---
+++ lists.py
@@ -0,0 +1,24 @@
+names = ['Foxh', 'Mwau', 'Akym']
+print(names)
+
+#accessinng first index
+print(names[0])
+
+#accessing last index
+print([-1])
+
+#inserting value at a specific index
+names.insert(3, 'Ali')
+print(names)
+
+#changing an element in a [articular index
+names[1] = 'Natalie'
+print(names)
+
+#pop removes last element
+names.pop()
+print(names)
+
+#deleting an element
+names.remove('Foxh')
+print(names)
