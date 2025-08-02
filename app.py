2831bd319f462e3e9ebe4d500c11524911ee58fd
Boniface Mwau <160026270+foxh79@users.noreply.github.com>
2025-08-02 06:50:05 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,10 @@
 import math
 
-name = input("Enter your official name: ")
+name = input("Enter your name: ")
 print(name)
 
 print(math.round(14))
 print(math.floor(14.77))
 print(math.ceil(14.77))
 print(math.exp(770))
+
