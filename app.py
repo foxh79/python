b67ad5957a2ceba5f8e765038dc83f985bf229a5
Boniface Mwau <160026270+foxh79@users.noreply.github.com>
2026-02-07 04:18:27 +0300

    Update input prompt and clean up app.py

--- app.py
+++ app.py
@@ -1,12 +1,13 @@
 import math
 
-name = input("Enter your name: ")
+name = input("Enter your official name: ")
 print(name)
 
 print(math.round(14))
 print(math.floor(14.77))
 print(math.ceil(14.77))
 print(math.exp(770))
+
 
 
 
